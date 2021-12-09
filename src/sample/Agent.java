dc7c96d82252c37e42f5edb77a286906266d42d4
Mattyboy84 <41299838+mattyboy84@users.noreply.github.com>
2021-12-09 15:12:00 +0000

    diffusion and fading is done - working on following/sensing mechanic

--- src/sample/Agent.java
+++ src/sample/Agent.java
@@ -4,7 +4,10 @@ import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.scene.image.WritableImage;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.RadialGradient;
 import javafx.util.Duration;
+
+import java.util.Random;
 
 public class Agent implements Runnable {
 
@@ -19,7 +22,14 @@ public class Agent implements Runnable {
     int width, height;
 
     public static int size = 0;
-
+    float mult = 1.5f * (30 / Main.fps);
+    float angleDelta = 45;
+    float angle;
+    float weightForward = 0, weightLeft = 0, weightRight = 0;
+    public int viewDis = 8;
+    int sensorSize = 4;
+    float turnSpeed = 2f * (30 / Main.fps);
+    Random random = new Random();
 
     WritableImage writableImage;
 
@@ -28,7 +38,7 @@ public class Agent implements Runnable {
         size++;
         this.position = new Vecc2f(startX, startY);
         //
-        float mult = 1.5f;
+
         this.velocity.random2D(mult);
         this.velocity.setMag(mult);
         //
@@ -37,35 +47,70 @@ public class Agent implements Runnable {
         //
         this.writableImage = writableImage;
         //
-        this.update = new Timeline(new KeyFrame(Duration.seconds((float) 1 / 30), event -> {
-
+        this.update = new Timeline(new KeyFrame(Duration.seconds((float) 1 / Main.fps), event -> {
+            //
+            steer();
+            //
             update(width, height);
-            try {
-                this.writableImage.getPixelWriter().setColor((int) Math.min(position.x,width), (int) Math.min(position.y,height), Color.WHITE);
-            } catch (IndexOutOfBoundsException e) {
-                System.out.println(threadName + " " + this.position);
-            }
-            //draw();
+            this.writableImage.getPixelWriter().setColor((int) Math.min(position.x, width), (int) Math.min(position.y, height), Color.rgb(255, 255, 255, 1));
 
         }));
         this.update.setCycleCount(Timeline.INDEFINITE);
         this.update.play();
 
+    }
 
+    private void steer() {
+        //System.out.println("-------------------");
+        weightForward = sense(0);
+        weightLeft = sense(-angleDelta);
+        weightLeft = sense(angleDelta);
+        //System.out.println("end -------------------");
+        //System.out.println("Left: " + weightLeft + " Middle: " + weightForward + " Right: " + weightRight);
+
+        if (weightForward < weightLeft && weightForward > weightRight) {
+
+        } else if (weightForward <= weightLeft && weightForward <= weightRight) {
+            this.velocity.fromAngle((this.velocity.toAngle()+ ((((random.nextInt(1)-0.5)/4))*turnSpeed)));
+        } else if (weightRight > weightLeft) {
+            this.velocity.fromAngle((this.velocity.toAngle()-(random.nextFloat()*(turnSpeed*2))));
+        } else if (weightLeft > weightRight) {
+            this.velocity.fromAngle((this.velocity.toAngle()+(random.nextFloat()*(turnSpeed*2))));
+
+        }
+    }
+
+    private float sense(float angleDelta) {
+        float a = 0f;
+        //
+        float angle = this.velocity.toAngle() + angleDelta;
+        Vecc2f sensorDir = new Vecc2f(Math.cos(Math.toRadians(angle)), Math.sin(Math.toRadians(angle)));
+        sensorDir.setMag(viewDis);
+        //System.out.println(sensorDir);
+        for (int i = (int) (position.x + sensorDir.x - (sensorSize / 2)); i < (int) (position.x + sensorDir.x + sensorSize + (sensorSize / 2)); i++) {
+            for (int j = (int) (position.y + sensorDir.y - (sensorSize / 2)); j < (int) (position.y + sensorDir.y + sensorSize + (sensorSize / 2)); j++) {
+                if (i > 0 && i < width && j > 0 && j < height) {
+                    a += writableImage.getPixelReader().getColor(i, j).getOpacity();
+                }
+            }
+        }
+
+        return a;
     }
 
     public void update(int width, int height) {
         this.position.add(this.velocity);
 
-        if ((int) this.position.x < 0 || (int) this.position.x >= width) {
-            this.position.sub(this.velocity);
-            this.velocity.x = (this.velocity.x * -1);
+        if ((int) this.position.x < 0 || (int) this.position.x >= width || (int) this.position.y < 0 || (int) this.position.y >= height) {
+            this.position.x = Math.min(width - 0.01f, Math.max(0, this.position.x));
+            this.position.y = Math.min(height - 0.01f, Math.max(0, this.position.y));
+            this.velocity.random2D(mult);
+            this.velocity.setMag(mult);
         }
-        if ((int) this.position.y < 0 || (int) this.position.y >= height) {
-            this.position.sub(this.velocity);
-            this.velocity.y = (this.velocity.y * -1);
+    }
 
-        }
+    public void update(double i) {
+        this.velocity.fromAngle(i);
 
     }
 
@@ -81,4 +126,6 @@ public class Agent implements Runnable {
             t.start();
         }
     }
+
+
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -16,13 +16,18 @@ import java.util.Random;
 
 public class Main extends Application {
 
-   int width = 320;
-   int height = 180;
+    int width = 320;
+    int height = 180;
     //int width = 640;
     //int height = 360;
+    //120x120 is a factor of 1080,1440,1920 - 120x120 grids on their own thread might work
+
+    final float diffusionSpread = 0.0005f;
+    public static float fps = 30;
+    public final float evaporateSpeed = 1f / (15f * (fps*1.3f));
 
     Group group = new Group();
-    Scene scene = new Scene(group, width, height);
+    Scene scene = new Scene(group, width, height, Color.BLACK);
 
     Canvas canvas = new Canvas(width, height);
     //
@@ -35,25 +40,48 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
+        System.out.println(evaporateSpeed);
 
+        scene.setOnKeyPressed(event -> {
+            switch (event.getCode()) {
+                case A:
+                    for (Agent agent : agents) {
+                        agent.velocity.set(0, 0);
+                    }
+                    break;
+                case B:
+                    for (Agent agent : agents) {
+                        agent.update(90);
+                    }
+                    break;
+            }
+        });
 
         group.getChildren().add(canvas);
         canvas.getGraphicsContext2D().getPixelWriter().setColor(100, 100, writableImage.getPixelReader().getColor(100, 100));
         canvas.relocate(0, 0);
         canvas.getGraphicsContext2D().fillRect(0, 0, width, height);
-        draw(Color.BLACK);
+        drawSetup();
 
         //for (int i = 0; i <10 ; i++) {
         //    writableImage.getPixelWriter().setColor(random.nextInt(width), random.nextInt(height),Color.BLACK );
         //}
 
-        for (int i = 0; i < 255; i++) {
-            agents.add(new Agent(width / 2, height / 2,writableImage));
+        for (int i = 0; i < 50; i++) {
+            agents.add(new Agent(width / 2, height / 2, writableImage));
             agents.get(agents.size() - 1).start();
         }
 
+        //for (int i = -1; i <2 ; i++) {
+        //    for (int j = -1; j <2 ; j++) {
+        //        System.out.print(i + " " + j);
+        //        System.out.print("   ");
+        //    }
+        //    System.out.println();
+        //}
 
-        this.drawer = new Timeline(new KeyFrame(Duration.seconds((float) 1 / 30), event -> {
+
+        this.drawer = new Timeline(new KeyFrame(Duration.seconds((float) 1 / fps), event -> {
             diffuse();
             draw();
         }));
@@ -70,6 +98,53 @@ public class Main extends Application {
     private void diffuse() {
 
 
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+
+                float originalValue = (float) writableImage.getPixelReader().getColor(i,j).getOpacity();
+
+                float blurResult = getAVGOpacity(i,j);
+
+                float diffuseValue = Lerp(originalValue,blurResult,0.8f);
+
+                float diffuseAndEvaporateValue = Math.max(diffuseValue-(evaporateSpeed),0);
+
+
+                writableImage.getPixelWriter().setColor(i, j, Color.rgb(255, 255, 255, diffuseAndEvaporateValue));
+            }
+        }
+    }
+
+    public static float Clamp01(float value) {
+        if (value < 0F)
+            return 0F;
+        else if (value > 1F)
+            return 1F;
+        else
+            return value;
+    }
+
+    // Interpolates between /a/ and /b/ by /t/. /t/ is clamped between 0 and 1.
+    public static float Lerp(float a, float b, float t) {
+        return a + (b - a) * Clamp01(t);
+    }
+
+    private float getAVGOpacity(int i, int j) {
+        float a = 0;
+
+        for (int k = -1; k <= 1; k++) {
+            for (int l = -1; l <= 1; l++) {
+                int SX = i + k;
+                int SY = j + l;
+                if (SX > 0 && SX < width && SY > 0 && SY < height) {
+                    a += writableImage.getPixelReader().getColor(SX, SY).getOpacity();
+                }
+            }
+        }
+
+        a /= 9f;
+
+        return a;
     }
 
     private void draw() {
@@ -80,16 +155,14 @@ public class Main extends Application {
         }
     }
 
-    private void draw(Color a) {
+    private void drawSetup() {
         for (int i = 0; i < width; i++) {
             for (int j = 0; j < height; j++) {
-                writableImage.getPixelWriter().setColor(i, j, Color.rgb(0,0,0,1));
-
-                canvas.getGraphicsContext2D().getPixelWriter().setColor(i, j, a);
+                writableImage.getPixelWriter().setColor(i, j, Color.rgb(255, 255, 255, 0));
+                //canvas.getGraphicsContext2D().getPixelWriter().setColor(i, j, a);
             }
         }
     }
-
 
     public static void main(String[] args) {
         launch(args);
